4a00090b61e54f0fa80781855ebb1db623e98247
Chrysanthi Kazakou <chrysanthi.work@gmail.com>
2025-06-15 19:03:18 +0300

    Add files via upload
    Lists - GetOnlyUpperCaseWords
    Implement the GetOnlyUpperCaseWords method, which given a collection of strings, returns a List with only those strings which contain only uppercase letters.
    
    The result collection should not contain duplicates.
    
    
    
    For example:
    
    for input List {"one", "TWO", "THREE", "four"} the result shall be {"TWO", "THREE"}
    
    for input List {"one", "TWO", "THREE", "four", "TWO"} the result shall be {"TWO", "THREE"} (the second "TWO" is ignored)
    
    for input List {"one", "TWO123", "THREE!&^", "four"} the result shall be an empty List because no word in this collection is built from only uppercase letters (digits and special characters are not letters at all).

---
+++ GetOnlyUpperCaseWords/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public List<string> GetOnlyUpperCaseWords(List<string> words)
+        {
+            List<string> result = new List<string>();
+            foreach (var word in words)
+            {
+                bool isUppercase = word == word.ToUpper();
+                bool onlyLetters = word.All(Char.IsLetter);
+                if (isUppercase && !result.Contains(word) && onlyLetters)
+                {
+                    result.Add(word);
+                }
+            }
+            return result;
+        }
+    }
+}
